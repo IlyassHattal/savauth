0c2d1c7456e7cd2d18a1f96b7b65435c5cfb2ad6
Ilyass Hattal <101954954+IlyassHattal@users.noreply.github.com>
2022-05-30 14:24:45 -0400

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://ilyasshattal:Teddybear123@cluster0.965rl.mongodb.net/cluster0?retryWrites=true&w=majority', 
+    'dbName': 'helloeli'
+};
